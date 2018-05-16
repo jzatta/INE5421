b8eb69488f25dc655388e4f9b07f1b15e89508ad
Leandro Perin <leandro.perino@gmail.com>
2018-05-16 14:11:13 -0300

    Epsilon production

--- T1/Grammar.java
+++ T1/Grammar.java
@@ -37,74 +37,77 @@ public class Grammar {
   }
 
   public static Automaton toNFA(Grammar G) {
-      LinkedList<String[]> gProductions = G.getProductions();
-      LinkedList<String> gNonTerminals = G.getNonTerminalSymbols();
-      LinkedList<String> nfaStates = new LinkedList<String>();
-      LinkedList<String> nfaFinalStates = new LinkedList<String>();
-      Automaton M = new Automaton();
-      int index = 0;
+    LinkedList<String[]> gProductions = G.getProductions();
+    LinkedList<String> gNonTerminals = G.getNonTerminalSymbols();
+    LinkedList<String> nfaStates = new LinkedList<String>();
+    LinkedList<String> nfaFinalStates = new LinkedList<String>();
+    Automaton M = new Automaton();
+    int index = 0;
 
-      for (String n: gNonTerminals) {
-        nfaStates.addLast("q" + index);
-        index++;
-      }
-
-      M.setInitialState(nfaStates.getFirst());
-
-      String lastX = "";
-      String lastx = "";
-      String[] lastT = new String[3];
-
-      for (String[] p: gProductions) {
-        String X = nfaStates.get(gNonTerminals.indexOf(p[0]));
-        String x = p[1];
-        String Y = "";
-
-        if (p[2].equals(Grammar.epsilon)) {
-          Y = "q" + index;
-          nfaFinalStates.addLast(Y);
-          index++;
-        } else {
-          Y = nfaStates.get(gNonTerminals.indexOf(p[2]));
-        }
-
-        if (X.equals(lastX) && x.equals(lastx)) {
-  //    	  M.getTransitions().getLast()[2] += "," + Y;
-      	  M.getTransitions().get(M.getTransitions().indexOf(lastT))[2] += "," + Y;
-      	  M.addTransition(X, x, Y);
-        } else {
-      	  M.addTransition(X, x, Y);
-      	  lastT = M.getTransitions().getLast();
-        }
-
-        lastX = X;
-        lastx = x;
-      }
-
-      LinkedList<String[]> toAdd = new LinkedList<String[]>();
-      for (String s: M.getSymbols()) {
-      	for (String state: M.getStates()) {
-      		Boolean exists = false;
-      		for (String[] t: M.getTransitions()) {
-      			if (t[0].equals(state) && t[1].equals(s)) {
-      				exists = true;
-      				break;
-      			}
-      		}
-      		if (!exists) {
-      			String[] t = {state, s};
-      			toAdd.addLast(t);
-      		}
-      	}
-      }
-      for (String[] t: toAdd) {
-      	M.addTransition(t[0], t[1], "__");
-      }
-
-      M.setFinalStates(nfaFinalStates);
-
-      return M;
+    for (String n: gNonTerminals) {
+      nfaStates.addLast("q" + index);
+      index++;
     }
+
+    M.setInitialState(nfaStates.getFirst());
+
+    String lastX = "";
+    String lastx = "";
+    String[] lastT = new String[3];
+
+    for (String[] p: gProductions) {
+      String X = nfaStates.get(gNonTerminals.indexOf(p[0]));
+      String x = p[1];
+      String Y = "";
+
+      if (x.equals(Grammar.epsilon) && X.equals(M.getInitialState())) {
+    	  nfaFinalStates.addLast(M.getInitialState());
+      } else {
+    	  if (p[2].equals(Grammar.epsilon)) {
+    	        Y = "q" + index;
+    	        nfaFinalStates.addLast(Y);
+    	        index++;
+    	      } else {
+    	        Y = nfaStates.get(gNonTerminals.indexOf(p[2]));
+    	      }
+
+    	      if (X.equals(lastX) && x.equals(lastx)) {
+    	    	  M.getTransitions().get(M.getTransitions().indexOf(lastT))[2] += "," + Y;
+    	    	  M.addTransition(X, x, Y);
+    	      } else {
+    	    	  M.addTransition(X, x, Y);
+    	    	  lastT = M.getTransitions().getLast();
+    	      }
+
+    	      lastX = X;
+    	      lastx = x;
+      }
+    }
+
+    LinkedList<String[]> toAdd = new LinkedList<String[]>();
+    for (String s: M.getSymbols()) {
+    	for (String state: M.getStates()) {
+    		Boolean exists = false;
+    		for (String[] t: M.getTransitions()) {
+    			if (t[0].equals(state) && t[1].equals(s)) {
+    				exists = true;
+    				break;
+    			}
+    		}
+    		if (!exists) {
+    			String[] t = {state, s};
+    			toAdd.addLast(t);
+    		}
+    	}
+    }
+    for (String[] t: toAdd) {
+    	M.addTransition(t[0], t[1], "__");
+    }
+
+    M.setFinalStates(nfaFinalStates);
+
+    return M;
+  }
 
   public String toString() {
     String ln = System.getProperty("line.separator");
